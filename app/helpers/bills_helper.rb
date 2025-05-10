ba7b660ffae78e231bd68e7e4300bd3671007386
Ian Withers <ian.neugent-withers@eatstreet.com>
2025-05-10 16:54:30 -0400

    feat: add currency decorator

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,2 +1,14 @@
 module BillsHelper
+  def currencyDecorator(input)
+    input = input.to_s
+    if input.length < 1
+      return "0.00"
+    elsif input.length == 1
+      return "0.0" + input
+    elsif input.length == 2
+      return "0." + input
+    else
+      return input[0..-2] + "." + input [-2..]
+    end
+  end
 end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -14,4 +14,22 @@
 class Bill < ApplicationRecord
   belongs_to :payment_source
   validates :name, :amount, :date_number, :payment_source_id, presence: true
+
+  def convertCurrencyForSubmit(input)
+  # we need to handle decimal type inputs in all variations, ex: "10.00", "10.2", "10."
+    # need to be converted to cent integer
+    if input.include?(".")
+      amount_parts = input.split(".")
+      ## check if any trailing zeros need to added
+      if amount_parts.last.length < 2
+      trailing_zeros = ""
+      (2 - amount_parts.last.length).times {trailing_zeros = trailing_zeros + "0" }
+      amount_parts.last = amount_parts.last + trailing_zeros
+      end 
+      return amount_parts.join("")
+    else 
+      return input + "00"
+    end 
+
+  end
 end
