786fe4c401c0cdefc86c8e169dfa0d351ee80044
Ian Withers <ian.neugent-withers@eatstreet.com>
2025-05-10 22:58:20 -0400

    style: bill index and show partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 PaymentSource.create(name: "Joint", payment_type: :checking_account)
 PaymentSource.create(name: "Personal", payment_type: :checking_account)
 PaymentSource.create(name: "Discover", payment_type: :credit_card)
+
+payment_source_ids = PaymentSource.all.map {|ps| ps.id}
+
+15.times do |i|
+  Bill.create(name: "Bill_#{i}", date_number: rand(31), amount: rand(500..20000), payment_source_id: payment_source_ids.sample)
+end
