f66e3d87b8c8a73ca6fa3dec9ad6b9eb5c3a5af7
Ian <mr.bowtie@Ians-MacBook-Pro.local>
2025-10-30 16:15:13 -0400

    refactor: use BillUtils for calculating bill_records

--- app/models/pay_period_breakdown.rb
+++ app/models/pay_period_breakdown.rb
@@ -11,16 +11,15 @@
 #  updated_at      :datetime         not null
 #
 class PayPeriodBreakdown < ApplicationRecord
+  validates :pay_date, :paycheck_amount, presence: true
 
-  validates :pay_date, paycheck_amount, presence: true
-
-  def sum_bills
-    bills.map(&:amount).sum
+  def bills
+    BillUtils.gatherInPeriod pay_date, next_pay_date - 1.day
   end
 
   def calculate
     results = {}
-    results[:leftover_funds] = paycheck_amount - sum_bills
+    results[:leftover_funds] = paycheck_amount - BillUtils.sum_records(bills)
     results[:credit_card_funds] = leftover_funds * 0.75
     results[:fun_money] = leftover_funds - credit_card_funds
     results[:individual_funds] = fun_money * 0.5
